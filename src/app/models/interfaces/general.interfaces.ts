eb662dba0e4d289b06bc0129dec4340075b53c7a
MaJoCar <carbajal.mj@outlook.com>
2019-11-14 17:46:50 -0600

    cards

--- src/app/models/interfaces/general.interfaces.ts
+++ src/app/models/interfaces/general.interfaces.ts
@@ -28,4 +28,28 @@ export interface DirectorioI {
     refFotos: string[];
     urlFotos: string[];
     visitas: number;
+    visitas_h_16_17: number;
+    visitas_h_18_24: number;
+    visitas_h_25_34: number;
+    visitas_h_35_44: number;
+    visitas_h_45_54: number;
+    visitas_h_55_mas: number;
+    visitas_m_16_17: number;
+    visitas_m_18_24: number;
+    visitas_m_25_34: number;
+    visitas_m_35_44: number;
+    visitas_m_45_54: number;
+    visitas_m_55_mas: number;
+    contactos_h_16_17: number;
+    contactos_h_18_24: number;
+    contactos_h_25_34: number;
+    contactos_h_35_44: number;
+    contactos_h_45_54: number;
+    contactos_h_55_mas: number;
+    contactos_m_16_17: number;
+    contactos_m_18_24: number;
+    contactos_m_25_34: number;
+    contactos_m_35_44: number;
+    contactos_m_45_54: number;
+    contactos_m_55_mas: number;
 }
--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { DirectorioI } from '../../models/interfaces/general.interfaces';
 
 @Component({
   selector: 'app-card',
@@ -6,10 +7,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
+  visitas: number;
+  contactos: number;
+  @Input() data: DirectorioI;
+  @Input() option: boolean;
+  constructor() {
 
-  constructor() { }
+  }
 
   ngOnInit() {
+    if (this.data) {
+      this.visitas = (
+        this.data.visitas_h_16_17 +
+        this.data.visitas_h_18_24 +
+        this.data.visitas_h_25_34 +
+        this.data.visitas_h_35_44 +
+        this.data.visitas_h_45_54 +
+        this.data.visitas_h_55_mas +
+        this.data.visitas_m_16_17 +
+        this.data.visitas_m_18_24 +
+        this.data.visitas_m_25_34 +
+        this.data.visitas_m_35_44 +
+        this.data.visitas_m_45_54 +
+        this.data.visitas_m_55_mas
+      );
+      this.contactos = (
+        this.data.contactos_h_16_17 +
+        this.data.contactos_h_18_24 +
+        this.data.contactos_h_25_34 +
+        this.data.contactos_h_35_44 +
+        this.data.contactos_h_45_54 +
+        this.data.contactos_h_55_mas +
+        this.data.contactos_m_16_17 +
+        this.data.contactos_m_18_24 +
+        this.data.contactos_m_25_34 +
+        this.data.contactos_m_35_44 +
+        this.data.contactos_m_45_54 +
+        this.data.contactos_m_55_mas
+      );
+    }
   }
 
 }
