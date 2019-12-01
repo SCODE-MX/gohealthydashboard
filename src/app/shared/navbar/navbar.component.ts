0e39b5295c653181bbffd9854c45beb3cb8688cc
alexpinkus <alexpinkus@hotmail.com>
2019-12-01 00:02:43 -0600

    Added dialog to check plan status

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,9 @@
+import { AuthService } from 'src/app/auth/auth.service';
+
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+
+import { PopupComponent } from '../popup/popup.component';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +11,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  animal: string;
+  name: string;
 
-  constructor() { }
+  constructor(public auth: AuthService, public dialog: MatDialog) { }
 
   ngOnInit() {
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(PopupComponent, {
+      width: '250px',
+      data: {name: this.name, animal: this.animal}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.animal = result;
+      console.log('this.animal :', this.animal);
+    });
   }
 
 }
