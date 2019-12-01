0f84a6e5a4c33c80fd431dae60876e0587f7ccf5
alexpinkus <alexpinkus@hotmail.com>
2019-12-01 00:06:20 -0600

    Added subscribe component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { ToastrModule } from 'ngx-toastr';
+import { environment } from 'src/environments/environment';
+
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
-import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
-import { AppRoutingModule } from './app-routing.module';
-import { SharedModule } from './shared/shared.module';
 import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireStorageModule } from '@angular/fire/storage';
-import { AngularFireAuthModule } from '@angular/fire/auth';
-import { DetalleComponent } from './views/detalle/detalle.component';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 import { MaterialModule } from './material.module';
-import { environment } from 'src/environments/environment';
-import { ToastrModule } from 'ngx-toastr';
+import { PopupComponent } from './shared/popup/popup.component';
+import { SharedModule } from './shared/shared.module';
+import { SubscribeComponent } from './views/subscribe/subscribe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthLayoutComponent,
-    AdminLayoutComponent
+    AdminLayoutComponent,
+    SubscribeComponent
   ],
   imports: [
     BrowserModule,
@@ -32,6 +36,9 @@ import { ToastrModule } from 'ngx-toastr';
     AngularFirestoreModule, // imports firebase/firestore, only needed for database features
     AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
     AngularFireStorageModule // imports firebase/storage only needed for storage features
+  ],
+  entryComponents: [
+    PopupComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
