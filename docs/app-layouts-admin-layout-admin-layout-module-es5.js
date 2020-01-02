b7e4883e26763eba68e0617bf965135c6702ff60
alexpinkus <alexpinkus@hotmail.com>
2020-01-02 13:13:23 -0600

    Added build for github pages

---
+++ docs/app-layouts-admin-layout-admin-layout-module-es5.js
@@ -0,0 +1,40 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["app-layouts-admin-layout-admin-layout-module"], {
+        /***/ "./src/app/layouts/admin-layout/admin-layout.module.ts": 
+        /*!*************************************************************!*\
+          !*** ./src/app/layouts/admin-layout/admin-layout.module.ts ***!
+          \*************************************************************/
+        /*! exports provided: AdminLayoutModule */
+        /***/ (function (module, __webpack_exports__, __webpack_require__) {
+            "use strict";
+            __webpack_require__.r(__webpack_exports__);
+            /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AdminLayoutModule", function () { return AdminLayoutModule; });
+            /* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+            /* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm2015/core.js");
+            /* harmony import */ var _angular_common__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/common */ "./node_modules/@angular/common/fesm2015/common.js");
+            /* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @angular/router */ "./node_modules/@angular/router/fesm2015/router.js");
+            /* harmony import */ var src_app_shared_shared_module__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! src/app/shared/shared.module */ "./src/app/shared/shared.module.ts");
+            var routes = [
+                { path: 'dashboard', loadChildren: function () { return __webpack_require__.e(/*! import() | views-dashboard-dashboard-module */ "views-dashboard-dashboard-module").then(__webpack_require__.bind(null, /*! ../../views/dashboard/dashboard.module */ "./src/app/views/dashboard/dashboard.module.ts")).then(function (m) { return m.DashboardModule; }); } },
+                { path: '', redirectTo: 'dashboard' },
+            ];
+            var AdminLayoutModule = /** @class */ (function () {
+                function AdminLayoutModule() {
+                }
+                return AdminLayoutModule;
+            }());
+            AdminLayoutModule = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+                Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
+                    declarations: [],
+                    imports: [
+                        _angular_common__WEBPACK_IMPORTED_MODULE_2__["CommonModule"],
+                        _angular_router__WEBPACK_IMPORTED_MODULE_3__["RouterModule"].forChild(routes),
+                        src_app_shared_shared_module__WEBPACK_IMPORTED_MODULE_4__["SharedModule"]
+                    ]
+                })
+            ], AdminLayoutModule);
+            /***/ 
+        })
+    }]);
+//# sourceMappingURL=app-layouts-admin-layout-admin-layout-module-es2015.js.map
+//# sourceMappingURL=app-layouts-admin-layout-admin-layout-module-es5.js.map
+//# sourceMappingURL=app-layouts-admin-layout-admin-layout-module-es5.js.map
---
+++ docs/app-layouts-auth-layout-auth-layout-module-es5.js
@@ -0,0 +1,37 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["app-layouts-auth-layout-auth-layout-module"], {
+        /***/ "./src/app/layouts/auth-layout/auth-layout.module.ts": 
+        /*!***********************************************************!*\
+          !*** ./src/app/layouts/auth-layout/auth-layout.module.ts ***!
+          \***********************************************************/
+        /*! exports provided: AuthLayoutModule */
+        /***/ (function (module, __webpack_exports__, __webpack_require__) {
+            "use strict";
+            __webpack_require__.r(__webpack_exports__);
+            /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AuthLayoutModule", function () { return AuthLayoutModule; });
+            /* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+            /* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm2015/core.js");
+            /* harmony import */ var _angular_common__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/common */ "./node_modules/@angular/common/fesm2015/common.js");
+            /* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @angular/router */ "./node_modules/@angular/router/fesm2015/router.js");
+            var routes = [
+                { path: 'login', loadChildren: function () { return __webpack_require__.e(/*! import() | views-login-login-module */ "views-login-login-module").then(__webpack_require__.bind(null, /*! ../../views/login/login.module */ "./src/app/views/login/login.module.ts")).then(function (m) { return m.LoginModule; }); } },
+            ];
+            var AuthLayoutModule = /** @class */ (function () {
+                function AuthLayoutModule() {
+                }
+                return AuthLayoutModule;
+            }());
+            AuthLayoutModule = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+                Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
+                    declarations: [],
+                    imports: [
+                        _angular_common__WEBPACK_IMPORTED_MODULE_2__["CommonModule"],
+                        _angular_router__WEBPACK_IMPORTED_MODULE_3__["RouterModule"].forChild(routes),
+                    ]
+                })
+            ], AuthLayoutModule);
+            /***/ 
+        })
+    }]);
+//# sourceMappingURL=app-layouts-auth-layout-auth-layout-module-es2015.js.map
+//# sourceMappingURL=app-layouts-auth-layout-auth-layout-module-es5.js.map
+//# sourceMappingURL=app-layouts-auth-layout-auth-layout-module-es5.js.map
