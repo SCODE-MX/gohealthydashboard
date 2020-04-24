f9283c0a8c3a2fff56e6e920a1cac26ac00a9449
alexpinkus <alexpinkus@hotmail.com>
2020-04-23 19:18:26 -0500

    Updated stripe prod key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,15 +1,15 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyCEO3194FbhWkkw3pOrGdbRCs1YknGMcJo',
-    authDomain: 'gohealthy-84b4c.firebaseapp.com',
-    databaseURL: 'https://gohealthy-84b4c.firebaseio.com',
-    projectId: 'gohealthy-84b4c',
-    storageBucket: 'gohealthy-84b4c.appspot.com',
-    messagingSenderId: '1069867771545',
-    appId: '1:1069867771545:web:7c3791b21b5eae82'
+    apiKey: "AIzaSyCEO3194FbhWkkw3pOrGdbRCs1YknGMcJo",
+    authDomain: "gohealthy-84b4c.firebaseapp.com",
+    databaseURL: "https://gohealthy-84b4c.firebaseio.com",
+    projectId: "gohealthy-84b4c",
+    storageBucket: "gohealthy-84b4c.appspot.com",
+    messagingSenderId: "1069867771545",
+    appId: "1:1069867771545:web:7c3791b21b5eae82",
   },
   stripe: {
-    key: 'pk_test_e3seNsumyGCb2APHEY4WLNZm00kNN3Emin'
+    key: "pk_live_5rtwa1uzWsFrBI8SqqMMDXow00qRgUGTt6",
   },
 };
